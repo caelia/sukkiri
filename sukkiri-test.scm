4350ca9f70ffa8107693ef9589d1ac5bc5b646f7
mgushee <matt@gushee.net>
2012-11-10 18:27:03 -0700

    Added support files.

---
+++ sukkiri-test.scm
@@ -0,0 +1,15 @@
+;;; sukkiri-test.scm -- Unit tests for the Sukkiri egg.
+;;; Copyright © 2012 by Matthew C. Gushee <matt@gushee.net>
+;;; This is open source software, released under the BSD license. See
+;;;   the accompanying LICENSE file for details.
+
+
+;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+;;; ------------------------------------------------------------------------
+
+;;; OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+;;; ========================================================================
+;;; ------------------------------------------------------------------------
+
+
