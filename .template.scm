48083595448e66698fea6ff2256bd3b98598c7a3
mgushee <matt@gushee.net>
2012-11-10 18:27:35 -0700

    Added source file template.

---
+++ .template.scm
@@ -0,0 +1,14 @@
+;;; {DESCRIPTION}
+;;; Copyright © 2012 by Matthew C. Gushee <matt@gushee.net>
+;;; This is open source software, released under the BSD license. See
+;;;   the accompanying LICENSE file for details.
+
+
+;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+;;; ------------------------------------------------------------------------
+
+;;; OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+;;; ========================================================================
+;;; ------------------------------------------------------------------------
+
