9d48801f834621114730d4c3136d147fc6020e24
mgushee <matt@gushee.net>
2012-11-22 19:08:54 -0700

    Basic vocabs API complete.

--- sukkiri-vocabs.scm
+++ sukkiri-vocabs.scm
@@ -10,6 +10,27 @@
         
         (use sukkiri-lib)
 
+(define (vocab-index-add! vocab-name terms)
+  (index-add! vocab-name terms prefix: "%VOCAB:"))
+
+(define (term-index-add! vocab term refs)
+  (let ((pfx (string-append "%VOCAB:" vocab ":")))
+    (index-add! term refs pfx)))
+
+(define (vocab-index-delete! vocab-name term)
+  (index-delete! vocab-name term "%VOCAB:"))
+
+(define (term-index-delete! vocab term refs)
+  (let ((pfx (string-append "%VOCAB:" vocab ":")))
+    (index-delete! term refs pfx)))
+
+(define (vocab-index-exists? vocab val)
+  (index-exists? vocab val "%VOCAB:"))
+
+(define (term-index-exists? vocab term ref)
+  (let ((pfx (string-append "%VOCAB:" vocab ":")))
+    (index-exists? vocab term pfx)))
+
 )
 
 ;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
