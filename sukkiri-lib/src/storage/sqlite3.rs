4ae0ae49b6062d2188ba591fea463e21a09c1475
Matt Gushee <matt@gushee.net>
2015-01-16 18:13:49 -0700

    Still not werking ...

--- sukkiri-lib/src/storage/sqlite3.rs
+++ sukkiri-lib/src/storage/sqlite3.rs
@@ -5,54 +5,38 @@ use rusqlite::SqliteConnection;
 // mod sql_queries;
 // mod base;
 
-pub struct SKSqliteStore<'a, 'b> {
+pub struct SKSqliteStore<'a> {
     path: &'a str,
-    conn: Option<SqliteConnection>
+    conn: Option<Box<SqliteConnection>>
 }
 
-impl<'a, 'b> SKSqliteStore<'a, 'b> {
+impl<'a> SKSqliteStore<'a> {
     pub fn new(file: &'a str) -> SKSqliteStore {
-        let store = SKSqliteStore { path: file, conn: &None };
+        let store = SKSqliteStore { path: file, conn: None };
         store
     }
 }
 
-impl<'a, 'b> SKStore for SKSqliteStore<'a, 'b> {
+impl<'a> SKStore for SKSqliteStore<'a> {
     fn init(&self) {
         println!("Initializing database.");
     }
     fn connect(&mut self) {
-        /*
-        let conn = match self.conn {
-            &Some(ref c) => c,
-            &None => {
-                &SqliteConnection::open(self.path).unwrap()
-            }
-        };
-        &Some(*conn);
-        */
-        /*
-        match self {
-            &mut SKSqliteStore { conn: Some(_), .. } => true,
-            &mut SKSqliteStore { conn: None, path: p } => {
-                /*
-                let c = SqliteConnection::open(p).unwrap();
-                self.conn = Some(&mut c);
-                */
-                self.conn = Some(&mut SqliteConnection::open(p).unwrap());
+        match self.conn {
+            Some(ref c) => true,
+            None => {
+                let c = SqliteConnection::open(self.path).unwrap();
+                self.conn = Some(Box::new(c));
                 false
             }
         };
-        */
-        if *self.conn == &None {
-            self.conn = &Some(SqliteConnection::open(self.path).unwrap());
-        }
         println!("Connected to {}.", self.path);
     }
     fn disconnect(&mut self) {
         match self.conn {
-            &Some(c) => { c.close(); self.conn = &None; true }
-            &None => false
+            // Some(ref c) => { c.close(); self.conn = None; true }
+            Some(ref c) => { let conn = *c; conn.close(); self.conn = None; true }
+            None => false
         };
         println!("Disconnected from {}.", self.path);
     }
