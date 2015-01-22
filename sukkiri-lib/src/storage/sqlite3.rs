a3e749c5cb84b080bf0b1a0aeaef710e8f13bf75
Matt Gushee <matt@gushee.net>
2015-01-22 12:07:29 -0700

    Fixed disconnect problem using std::Option::take.

--- sukkiri-lib/src/storage/sqlite3.rs
+++ sukkiri-lib/src/storage/sqlite3.rs
@@ -5,56 +5,34 @@ use rusqlite::SqliteConnection;
 // mod sql_queries;
 // mod base;
 
-pub struct SKSqliteStore<'a, 'b> {
+pub struct SKSqliteStore<'a> {
     path: &'a str,
     conn: Option<SqliteConnection>
 }
 
-impl<'a, 'b> SKSqliteStore<'a, 'b> {
+impl<'a> SKSqliteStore<'a> {
     pub fn new(file: &'a str) -> SKSqliteStore {
-        let store = SKSqliteStore { path: file, conn: &None };
+        let store = SKSqliteStore { path: file, conn: None };
         store
     }
 }
 
-impl<'a, 'b> SKStore for SKSqliteStore<'a, 'b> {
+impl<'a> SKStore for SKSqliteStore<'a> {
     fn init(&self) {
         println!("Initializing database.");
     }
     fn connect(&mut self) {
-        /*
-        let conn = match self.conn {
-            &Some(ref c) => c,
-            &None => {
-                &SqliteConnection::open(self.path).unwrap()
-            }
+        match self.conn {
+            Some(_) => (),
+            None => self.conn = Some(SqliteConnection::open(self.path).unwrap())
         };
-        &Some(*conn);
-        */
-        /*
-        match self {
-            &mut SKSqliteStore { conn: Some(_), .. } => true,
-            &mut SKSqliteStore { conn: None, path: p } => {
-                /*
-                let c = SqliteConnection::open(p).unwrap();
-                self.conn = Some(&mut c);
-                */
-                self.conn = Some(&mut SqliteConnection::open(p).unwrap());
-                false
-            }
-        };
-        */
-        if *self.conn == &None {
-            self.conn = &Some(SqliteConnection::open(self.path).unwrap());
-        }
-        println!("Connected to {}.", self.path);
     }
     fn disconnect(&mut self) {
-        match self.conn {
-            &Some(c) => { c.close(); self.conn = &None; true }
-            &None => false
+        let conn = self.conn.take();
+        match conn {
+            Some(c) => c.close().unwrap(),
+            None => ()
         };
-        println!("Disconnected from {}.", self.path);
     }
 }
 
