14f3799396a6146db60ab7345e4539455cb008d1
Matt Gushee <matt@gushee.net>
2015-03-02 14:34:47 -0700

    Minor updates to two modules.

--- sukkiri-query.scm
+++ sukkiri-query.scm
@@ -1,9 +1,28 @@
-;;; {DESCRIPTION}
-;;;   Copyright © 2014 by Matthew C. Gushee <matt@gushee.net>
-;;;   This program is open-source software, released under the GNU General
-;;;   Public License v3. See the accompanying LICENSE file for details.
+;;; sukkiri-query.scm -- Query processor for Sukkiri
+;;;   Copyright © 2015 by Matthew C. Gushee <matt@gushee.net>
+;;;   This program is open-source software, released under the BSD
+;;;   license. See the accompanying LICENSE file for details.
 
+(module sukkiri-query
+        *
+        (import scheme chicken)
+        (use sukkiri-base)
+        (use sukkiri-store)
+        (use sukkiri-data)
 
+;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+;;; ------------------------------------------------------------------------
+
+(define (typedef))
+
+(define (create))
+
+(define (update))
+
+;;; OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+(define ())
+)
 ;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
 ;;; ------------------------------------------------------------------------
 
--- sukkiri-store.scm
+++ sukkiri-store.scm
@@ -1,7 +1,7 @@
 ;;; sukkiri-store.scm -- SQLite3 interface for Sukkiri.
 ;;;   Copyright © 2014 by Matthew C. Gushee <matt@gushee.net>
-;;;   This program is open-source software, released under the GNU General
-;;;   Public License v3. See the accompanying LICENSE file for details.
+;;;   This program is open-source software, released under the BSD
+;;;   license. See the accompanying LICENSE file for details.
 
 (module sukkiri-store
         *
