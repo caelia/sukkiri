18a483cde6f8b761bfb482b618ed9bd093844585
Matt Gushee <matt@gushee.net>
2015-03-11 19:08:07 -0600

    Added command interpreter module.

---
+++ sukkiri-command.scm
@@ -0,0 +1,23 @@
+;;; sukkiri-command.scm -- command parser/interpreter for Sukkiri
+;;;   Copyright © 2015 by Matthew C. Gushee <matt@gushee.net>
+;;;   This program is open-source software, released under the
+;;;   BSD License. See the accompanying LICENSE file for details.
+
+(module sukkiri-command
+        *
+        (import scheme chicken)
+        (use comparse)
+        (use sukkiri-store)
+        (use sukkiri-data)
+
+
+) ; END MODULE
+
+;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
+;;; ------------------------------------------------------------------------
+
+;;; OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+
+;;; ========================================================================
+;;; ------------------------------------------------------------------------
+
