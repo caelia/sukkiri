97d0f0f34e69405e407f77e8654dba812e179f5b
Matt Gushee <matt@gushee.net>
2015-03-11 18:33:53 -0600

    Updated template file to reflect license change.

--- .template.scm
+++ .template.scm
@@ -1,7 +1,7 @@
 ;;; {DESCRIPTION}
-;;;   Copyright © 2014 by Matthew C. Gushee <matt@gushee.net>
-;;;   This program is open-source software, released under the GNU General
-;;;   Public License v3. See the accompanying LICENSE file for details.
+;;;   Copyright © 2015 by Matthew C. Gushee <matt@gushee.net>
+;;;   This program is open-source software, released under the
+;;;   BSD License. See the accompanying LICENSE file for details.
 
 
 ;;; IIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII
